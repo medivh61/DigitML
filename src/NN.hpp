aa07558d8e647c6b73960c1c2fb484f6ebc13283
Артём <48670130+medivh61@users.noreply.github.com>
2023-09-13 14:10:10 +0300

    Update NN.hpp

--- src/NN.hpp
+++ src/NN.hpp
@@ -17,7 +17,7 @@ class NeuralNetwork {
 
         std::vector<double> sigmoid(const std::vector<double>& x);
         std::vector<double> sigmoid_prime(const std::vector<double>& x);
-        std::vector<double> isrlu(const std::vector<double>& x, dobule aplha);
+        std::vector<double> isrlu(const std::vector<double>& x, double aplha);
 
 		Matrix<double> weight_init(double max_weight, unsigned int width, unsigned int height);
 
