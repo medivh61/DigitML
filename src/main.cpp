a0f72b01dcfea9180e9aed29b8e23b78a6830e3d
ToriaDem <99827990+DementienkoViktoria@users.noreply.github.com>
2023-09-26 21:09:53 +0300

    Update main.cpp
    proba tests

--- src/main.cpp
+++ src/main.cpp
@@ -36,50 +36,42 @@ const double calculate_accuracy(const Matrix<unsigned char>& images, const Matri
 }
 
 void tests(int count){
+    double sum_train = 0;
+    double sum_test = 0;
     for(int i =0; i <= count; i++){
+        Matrix<unsigned char> images_train(0, 0);
+   
+        Matrix<unsigned char> labels_train(0, 0);
+   
+        load_dataset(images_train, labels_train, "data/train-images-idx3-ubyte", "data/train-labels-idx1-ubyte");
 
+   
+        Matrix<unsigned char> images_test(0, 0);
+        Matrix<unsigned char> labels_test(0, 0);
+        load_dataset(images_test, labels_test, "data/t10k-images-idx3-ubyte", "data/t10k-labels-idx1-ubyte");
+
+        NeuralNetwork n;
+
+        const unsigned int num_iterations = 5;
+        n.train(num_iterations, images_train, labels_train);
+
+        const double accuracy_train = calculate_accuracy(images_train, labels_train, n);
+        const double accuracy_test = calculate_accuracy(images_test, labels_test, n);
+        
+        sum_train += accuracy_train;
+        sum_test += accuracy_test;
+        
+        printf("Accuracy on training data: %f\n", accuracy_train);
+        printf("Accuracy on test data: %f\n", accuracy_test);
+    
     };
+    printf(" ----------------------------------------\n Average accuracy of train data: %f\n", sum_train/count);
+    printf(" Average accuracy of test data: %f\n", sum_test/count);
 }
 
 int main() {
-   // tests(5);
-    Matrix<unsigned char> images_train(0, 0);
-    Matrix<unsigned char> labels_train(0, 0);
-    load_dataset(images_train, labels_train, "data/train-images-idx3-ubyte", "data/train-labels-idx1-ubyte");
-
-    Matrix<unsigned char> images_test(0, 0);
-    Matrix<unsigned char> labels_test(0, 0);
-    load_dataset(images_test, labels_test, "data/t10k-images-idx3-ubyte", "data/t10k-labels-idx1-ubyte");
-
-    NeuralNetwork n;
-
-      for (int i = 0; i < 10; ++i) {
-        Example e;
-        for (int j = 0; j < 28*28; ++j) {
-            e.data[j] = images_train[i][j];
-        }
-        e.label = labels_train[i][0];
-        debug(e);
-        printf("Guess: %d\n", n.compute(e));
-    }
-    for (int i = 0; i < 10; ++i) {
-        Example e;
-        for (int j = 0; j < 28*28; ++j) {
-            e.data[j] = images_test[i][j];
-        }
-        e.label = labels_test[i][0];
-        debug(e);
-        printf("Guess: %d\n", n.compute(e));
-    }
-    const unsigned int num_iterations = 5;
-    n.train(num_iterations, images_train, labels_train);
-
-    const double accuracy_train = calculate_accuracy(images_train, labels_train, n);
-    const double accuracy_test = calculate_accuracy(images_test, labels_test, n);
-
-    printf("Accuracy on training data: %f\n", accuracy_train);
-    printf("Accuracy on test data: %f\n", accuracy_test);
-
+   tests(6);
+   
    
     return 0;
 }
