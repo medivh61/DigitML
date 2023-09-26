e45a9c397c9ad4d0b9a751cec535a33b09c8cb0c
ToriaDem <99827990+DementienkoViktoria@users.noreply.github.com>
2023-09-27 01:27:15 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -91,7 +91,7 @@ TEST(FunctionTesting, testSoftSign1){
 
 TEST(FunctionTesting, testSoftSign2){
     EXPECT_NEAR(soft(0.15),0.13,1e-2);
-    EXPECT_NEAR(soft(0.59),0,37106,1e-2);
+    EXPECT_NEAR(soft(0.59),0.37106,1e-2);
     EXPECT_NEAR(soft(-0.9),-0.474,1e-2);
 }
 
